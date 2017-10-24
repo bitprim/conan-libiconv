9002ebbd70d99c872d00ae4f6628a108f35bf61e
SSE4 <tomskside@gmail.com>
2017-10-24 13:59:17 +0700

    - minimal working prototype (tested on OSX)

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,8 @@
-from conans import ConanFile, tools, os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+import os
 
 
 class LibiconvConan(ConanFile):
@@ -9,20 +13,23 @@ class LibiconvConan(ConanFile):
     url = "https://github.com/bincrafters/conan-libiconv"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=True"
+    default_options = "shared=False"
     lib_short_name = "libiconv"
     archive_name = "{0}-{1}".format(lib_short_name, version)
+    install_dir = "libiconv-install"
             
     def source(self):
         source_url = "https://ftp.gnu.org/gnu/libiconv"
         tools.get("{0}/{1}.tar.gz".format(source_url, self.archive_name))
                 
-    def build(self):   
-        #if self.settings.os == "Linux":
-        #if self.settings.os == "Darwin":
+    def build(self):
         if self.settings.os != "Windows":
             env_build = AutoToolsBuildEnvironment(self)
-            configure_args = ['--prefix=%s' % self.install_dir]
+            configure_args = ['--prefix=%s' % os.path.abspath(self.install_dir)]
+            if self.options.shared:
+                configure_args.extend(['--disable-static', '--enable-shared'])
+            else:
+                configure_args.extend(['--enable-static', '--disable-shared'])
             with tools.chdir(self.archive_name):
                 env_build.configure(args=configure_args)
                 env_build.make()
@@ -30,13 +37,22 @@ class LibiconvConan(ConanFile):
     
     def package(self):
         self.copy("COPYING", dst=".", src=self.archive_name)
-        self.copy("*.h", dst="include", src=os.path.join(self.archive_name, "include"))
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so*", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src=os.path.join(self.install_dir, "include"))
+        if str(self.settings.os) in ["Macos", "iOS", "watchOS", "tvOS"]:
+            if self.options.shared:
+                self.copy("*.dylib", dst="lib", src=os.path.join(self.install_dir, "lib"), keep_path=False)
+            else:
+                self.copy("*.a", dst="lib", src=os.path.join(self.install_dir, "lib"), keep_path=False)
+        elif str(self.settings.os) in ["Linux", "Android"]:
+            if self.options.shared:
+                self.copy("*.so*", dst="lib", src=os.path.join(self.install_dir, "lib"), keep_path=False)
+            else:
+                self.copy("*.a", dst="lib", src=os.path.join(self.install_dir, "lib"), keep_path=False)
+        elif self.settings.os == "Windows":
+            self.copy("*.lib", dst="lib", src=os.path.join(self.install_dir, "lib"), keep_path=False)
+            if self.options.shared:
+                self.copy("*.dll", dst="bin", src=os.path.join(self.install_dir, "bin"), keep_path=False)
         
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-        self.env_info.path.append(path.join(self.package_folder, "bin"))
+        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
