bc3fe9ce857ba7107c9ed63dde0a1a4ee532bb9a
theirix <theirix@gmail.com>
2018-01-04 21:02:28 +0300

    Updated test package [skip ci]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,4 +1,7 @@
-from conans import ConanFile, CMake, tools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools, RunEnvironment
 import os
 
 
@@ -16,4 +19,11 @@ class TestPackageConan(ConanFile):
         self.copy("*", dst="bin", src="lib")
 
     def test(self):
-        self.run(os.path.join("bin","test_package"))
+        with tools.environment_append(RunEnvironment(self).vars):
+            bin_path = os.path.join("bin", "test_package")
+            if self.settings.os == "Windows":
+                self.run(bin_path)
+            elif self.settings.os == "Macos":
+                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+            else:
+                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
