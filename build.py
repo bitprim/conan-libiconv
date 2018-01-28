9865f6002ec2172fada01c4cb16d1b3292c7c384
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-01-28 12:40:22 -0300

    fixes

--- build.py
+++ build.py
@@ -1,18 +1,40 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default, build_shared
-import platform
+
+from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
+    builder = ConanMultiPackager(username="bitprim", channel="stable", archs=["x86_64"])
+    builder.add_common_builds(shared_option_name="libiconv:shared", pure_c=False)
 
-    builder = build_template_default.get_builder(pure_c=False)
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if (settings["build_type"] == "Release" or settings["build_type"] == "Debug") \
+                and not options["libiconv:shared"]:
 
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        if build_shared.get_os() == "Windows":
-            build_requires.update({"*": ["cygwin_installer/2.9.0@bincrafters/stable"]})
-    
+            filtered_builds.append([settings, options, env_vars, build_requires])
+
+    builder.builds = filtered_builds
     builder.run()
+
+
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+
+# from bincrafters import build_template_default, build_shared
+# import platform
+
+# if __name__ == "__main__":
+
+#     builder = build_template_default.get_builder(pure_c=False)
+
+#     for settings, options, env_vars, build_requires, reference in builder.items:
+#         if build_shared.get_os() == "Windows":
+#             build_requires.update({"*": ["cygwin_installer/2.9.0@bitprim/stable"]})
+    
+#     builder.run()
 
     
