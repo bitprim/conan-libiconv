eab71a8495b765c86e806f7a71c2ad603d46713a
Fernando pelliccioni <fpelliccioni@gmail.com>
2018-01-29 19:02:43 -0300

    fixes for mingw

--- conanfile.py
+++ conanfile.py
@@ -30,12 +30,60 @@ class LibiconvConan(ConanFile):
         source_url = "https://ftp.gnu.org/gnu/libiconv"
         tools.get("{0}/{1}.tar.gz".format(source_url, self.archive_name))
 
+    def build(self):
+        if self.settings.os == "Windows":
+            cygwin_bin = self.deps_env_info['cygwin_installer'].CYGWIN_BIN
+            with tools.environment_append({'PATH': [cygwin_bin],
+                                            'CONAN_BASH_PATH': '%s/bash.exe' % cygwin_bin}):
+                if self.settings.compiler == "Visual Studio":
+                    with tools.vcvars(self.settings):
+                        self.build_autotools()
+                elif self.settings.compiler == "gcc":
+                    # self.build_mingw()
+                    self.build_autotools()
+                else:
+                    # TODO : clang on Windows and others
+                    raise Exception("unsupported build")
+        else:
+            self.build_autotools()
+
+        # if self.settings.os == "Windows":
+        #     if self.settings.compiler == "Visual Studio":
+        #         cygwin_bin = self.deps_env_info['cygwin_installer'].CYGWIN_BIN
+        #         with tools.environment_append({'PATH': [cygwin_bin],
+        #                                        'CONAN_BASH_PATH': '%s/bash.exe' % cygwin_bin}):
+        #             with tools.vcvars(self.settings):
+        #                 self.build_autotools()
+        #     elif self.settings.compiler == "gcc":
+        #         # self.build_mingw()
+        #         self.build_autotools()
+        #     else:
+        #         # TODO : clang on Windows and others
+        #         raise Exception("unsupported build")
+        # else:
+        #     self.build_autotools()
+
+    def package(self):
+        self.copy(os.path.join(self.archive_name, "COPYING.LIB"), dst="licenses", ignore_case=True, keep_path=False)
+
+    def package_info(self):
+        if self.settings.os == "Windows" and self.options.shared:
+            self.cpp_info.libs = ['iconv.dll.lib']
+        else:
+            self.cpp_info.libs = ['iconv']
+        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+
+
+
     def build_autotools(self):
         prefix = os.path.abspath(self.package_folder)
         win_bash = False
-        if self.settings.compiler == 'Visual Studio':
-            prefix = tools.unix_path(prefix)
-            win_bash = True
+
+        # if self.settings.compiler == 'Visual Studio':
+        #     prefix = tools.unix_path(prefix)
+        #     win_bash = True
+        prefix = tools.unix_path(prefix)
+        win_bash = True
 
         env_build = AutoToolsBuildEnvironment(self, win_bash=win_bash)
         env_build.fpic = self.options.fPIC
@@ -85,29 +133,3 @@ class LibiconvConan(ConanFile):
 
     def build_mingw(self):
         raise Exception("not implemented")
-
-    def build(self):
-        if self.settings.os == "Windows":
-            if self.settings.compiler == "Visual Studio":
-                cygwin_bin = self.deps_env_info['cygwin_installer'].CYGWIN_BIN
-                with tools.environment_append({'PATH': [cygwin_bin],
-                                               'CONAN_BASH_PATH': '%s/bash.exe' % cygwin_bin}):
-                    with tools.vcvars(self.settings):
-                        self.build_autotools()
-            elif self.settings.compiler == "gcc":
-                self.build_mingw()
-            else:
-                # TODO : clang on Windows and others
-                raise Exception("unsupported build")
-        else:
-            self.build_autotools()
-
-    def package(self):
-        self.copy(os.path.join(self.archive_name, "COPYING.LIB"), dst="licenses", ignore_case=True, keep_path=False)
-
-    def package_info(self):
-        if self.settings.os == "Windows" and self.options.shared:
-            self.cpp_info.libs = ['iconv.dll.lib']
-        else:
-            self.cpp_info.libs = ['iconv']
-        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
